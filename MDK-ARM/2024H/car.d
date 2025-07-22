1e2a245863fcdd3a2e543d12fd6401aa9eaa8682
谭大潮 <3961443145@qq.com>
2025-07-22 11:53:29 +0800

    框架

---
+++ MDK-ARM/2024H/car.d
@@ -0,0 +1,13 @@
+2024h\car.o: ..\App\Car.c
+2024h\car.o: ..\App\Car.h
+2024h\car.o: ../Middle/FreeRTOS/include/FreeRTOS.h
+2024h\car.o: D:\Keli5\ARM\ARMCC\Bin\..\include\stddef.h
+2024h\car.o: D:\Keli5\ARM\ARMCC\Bin\..\include\stdint.h
+2024h\car.o: ../Core/Inc/FreeRTOSConfig.h
+2024h\car.o: ../Middle/FreeRTOS/include/projdefs.h
+2024h\car.o: ../Middle/FreeRTOS/include/portable.h
+2024h\car.o: ../Middle/FreeRTOS/include/deprecated_definitions.h
+2024h\car.o: ../Middle/FreeRTOS/portable/portmacro.h
+2024h\car.o: ../Middle/FreeRTOS/include/mpu_wrappers.h
+2024h\car.o: ../Middle/FreeRTOS/include/task.h
+2024h\car.o: ../Middle/FreeRTOS/include/list.h
